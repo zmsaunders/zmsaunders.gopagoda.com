443562f1aed20af6e1f99bca7554787ddcb34b56
Zach Saunders <zachary.saunders@gmail.com>
2013-06-27 14:37:13 -0400

    User Table migration and .sqlite file blocked from git.

---
+++ app/database/migrations/2013_06_27_183250_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('first_name', 100);
+			$table->string('last_name', 100);
+			$table->string('email', 255);
+			$table->string('password', 100);
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
